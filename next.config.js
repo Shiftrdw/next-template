9d054484943cbb76a0b6d5f0a4bb1d26d52f8e87
Shiftrdw <50456402+Shiftrdw@users.noreply.github.com>
2024-07-22 16:58:16 +0800

    build: change/migrate to next 14

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const path = require('path');
 
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
 };
 
 module.exports = nextConfig;
